4f780f0401e4de71efee4f7d90d707fc136f2bcf
cibarba <cibarba19@gmail.com>
2017-05-09 19:52:51 -0500

    Commit dafla.dafla 09.05.17.19:52

---
+++ app/src/main/java/dafla/dafla/PhotoDetailsActivity.java
@@ -0,0 +1,13 @@
+package dafla.dafla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_details);
+    }
+}
